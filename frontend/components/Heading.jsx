51c026a0afc8080fd8f8fe4831fc991160c0e775
Achin-Agarwal <achinagarwal9044@gmail.com>
2024-12-25 18:43:34 +0530

    Add Tailwind CSS setup and create Home and Heading components: implement Tailwind configuration, create Heading component with styles, and set up Home page to include Heading.

---
+++ frontend/components/Heading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Heading.css'
+
+const Heading = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Heading
