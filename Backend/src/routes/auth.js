5a35731a7cd5f44df58cc6b11d894037a8305e51
Devansh <devansh28112004@gmail.com>
2025-01-06 21:02:32 +0530

    Refactor Google authentication route; update role validation and improve token handling

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -21,17 +21,19 @@ router.post("/google", async (req, res, next) => {
     if (!role)
         res.status(400).json({ message: "Role is required" });
 
-    if (role !== "buyer" && role !== "seller")
-        res.status(400).json({ message: "Role must be either buyer or seller" });
+    // if (role !== "buyer" && role !== "seller")
+    //     res.status(400).json({ message: "Role must be either buyer or seller" });
 
     const googleRes = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(googleRes.tokens);
     const userRes = await axios.get(
-        `https://www.googleapis.com/oauth2/v1/buyerinfo?alt=json&access_token=${googleRes.tokens.access_token}`
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
     );
     const { email, name, picture } = userRes.data;
+    console.log("*********************************************************************************************************************************");
     console.log("userRes: ");
     console.log(userRes);
+    console.log("*********************************************************************************************************************************");
 
 
     if (role === "buyer") {
@@ -61,8 +63,8 @@ router.post("/google", async (req, res, next) => {
     }
     const { _id } = buyer;
     const token = jwt.sign({ _id, email },
-        process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_TIMEOUT,
+        config.jwt.secret, {
+        expiresIn: config.jwt.timeout,
     });
     res.status(200).json({
         message: 'success',
--- Backend/src/utils/googleClient.js
+++ Backend/src/utils/googleClient.js
@@ -1,7 +1,7 @@
 import { google } from 'googleapis';
 import config from '../config/config.js';
-const GOOGLE_CLIENT_ID = config.google.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = config.google.GOOGLE_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = config.google.clientId;
+const GOOGLE_CLIENT_SECRET = config.google.clientSecret;
 
 export const oauth2Client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
