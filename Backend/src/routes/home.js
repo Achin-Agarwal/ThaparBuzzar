770c2f0762989de3503f359167aef7e146de305f
Devansh <devansh28112004@gmail.com>
2025-01-14 18:03:54 +0530

    Comment out the products by category route and modify products retrieval to fetch all products

--- Backend/src/routes/home.js
+++ Backend/src/routes/home.js
@@ -13,19 +13,20 @@ router.get("/bestsellers", safeHandler(async (req, res) => {
     }));
     return res.json(bestsellers);
 }));
-router.get("/products/:category", safeHandler(async (req, res) => {
-    const { category } = req.params;
-    const products = await Product.find({ category }).exec();
-    return res.json(products);
-}));
+// router.get("/products/:category", safeHandler(async (req, res) => {
+//     const { category } = req.params;
+//     const products = await Product.find({ category }).exec();
+//     return res.json(products);
+// }));
 router.get("/products", safeHandler(async (req, res) => {
-    const products = await Product.find({ discountedPrice: { $lte: 0 } }).exec();
+    const products = await Product.find().exec();
    return res.json(products);
 }));
 router.get("/discountedproducts",safeHandler(async (req,res)=>{
    const products= await Product.find({ discountedPrice: { $gt: 0 } }).exec();
     return res.json(products);
 }));
+
 
 
 export default router;
